e90b94db89dce5eb042fdcb4773e5f0d7264f29d
AzouKr <69352658+AzouKr@users.noreply.github.com>
2021-08-21 19:46:58 +0100

    added some routes

--- backend/Controllers/imdbController.js
+++ backend/Controllers/imdbController.js
@@ -1,21 +1,40 @@
-const { response } = require('express');
-const imdb = require('imdb-api')
+const Axios = require('axios')
+
 
 
 module.exports = {
-    Search: async (req, res) => {
+  Popular: async (req, res) => {
 
-        imdb.search({
-            name: 'titanic'
-          }, {
-            apiKey: 'foo'
-          }).then((response) => {
-            res.send(response);
-        }).catch((e) => {
-            res.send(e);
-        });
+    Axios.get("https://api.themoviedb.org/3/movie/popular?api_key="+ process.env.api_key).then((response) => {
+      res.send(response.data);
+    });
 
-    }
+    },
+
+    Latest: async (req, res) => {
+
+      Axios.get("https://api.themoviedb.org/3/movie/latest?api_key="+ process.env.api_key).then((response) => {
+        res.send(response.data);
+      });
+  
+      },
+
+    Top_rated: async (req, res) => {
+
+      Axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key="+ process.env.api_key).then((response) => {
+        res.send(response.data);
+      });
+    
+      },
+
+    Now_playing: async (req, res) => {
+
+      Axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key="+ process.env.api_key).then((response) => {
+        res.send(response.data);
+      });
+      
+    },
+  
 
 
 }
--- backend/routes/imdb-routes.js
+++ backend/routes/imdb-routes.js
@@ -3,6 +3,19 @@ const controller = require("../Controllers/imdbController");
 
 
 // *************************** Search **************************************
-router.get("/search", controller.Search);
+
+// Get a list of the current popular movies
+router.get("/popular", controller.Popular);
+
+// Get the most newly created movie.
+router.get("/latest", controller.Latest);
+
+// Get the top rated movies.
+router.get("/top_rated", controller.Top_rated);
+
+// Get a list of movies in theatres.
+router.get("/now_playing", controller.Now_playing);
+
+
 
 module.exports = router;
